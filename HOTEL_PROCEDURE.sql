c8eac5f5d009a583168d7cf6a622b26efe425785
rensodalin <rrensodalin@gmail.com>
2025-04-06 14:15:35 +0700

    Add new project files and removed old report

--- HOTEL_PROCEDURE.sql
+++ HOTEL_PROCEDURE.sql
@@ -1,5 +1,5 @@
 -- Procedure add new room
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE add_new_room(
 	    IN bed_count INT,
 	    IN floor_number INT,
@@ -12,31 +12,29 @@
 	    INSERT INTO Room (bed_count, floor_number, price_per_night, room_type, discount, status)
 	    VALUES (bed_count, floor_number, price_per_night, room_type, discount, status);
 	END //
-	DELIMITER ;
+DELIMITER ;
 
 -- Procedure update room status
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE update_room_status(IN room_id INT, IN new_status VARCHAR(50))
 	BEGIN
-	    UPDATE Room 
+	    UPDATE Room r 
 	    SET status = new_status 
-	    WHERE room_id = room_id;
+	    WHERE r.room_id = room_id;
 	END //
-	DELIMITER ;
-
+DELIMITER ;
+call update_room_status(1, "Occupied");
 -- Procedure delete room
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE delete_room(IN room_id INT)
 	BEGIN
 	    DELETE FROM Room WHERE room_id = room_id;
 	END //
-	DELIMITER ;
-
-
+DELIMITER ;
 
 -- Procedure add new GUEST
 	desc guest;
-	delimiter //
+delimiter //
 	create procedure add_new_guest(
 		in first_name varchar(50),
 		in last_name varchar(50),
@@ -49,12 +47,11 @@
 		insert into guest (first_name, last_name, address, date_of_birth, date_of_register, phone_number, email)
 		values(first_name, last_name, address, date_of_birth, curdate(), phone_number, email);
 	end //
-	delimiter ;
-	
+delimiter ;
 	call add_new_guest('Leng', 'Menghan', 'Kandal', '2005-03-24', '012345678', 'han@gmail.com');
 
 -- Procedure to update guest
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE update_guest(
 	    IN guest_id INT,
 	    IN first_name VARCHAR(50),
@@ -74,19 +71,19 @@
 	        date_of_birth = date_of_birth
 	    WHERE guest_id = guest_id;
 	END //
-	DELIMITER ;
+DELIMITER ;
 
 -- Procedure to delete guest
-	DELIMITER //
-	CREATE PROCEDURE delete_room(IN room_id INT)
+DELIMITER //
+	CREATE PROCEDURE delete_guest(IN guest_id INT)
 	BEGIN
-	    DELETE FROM Room WHERE room_id = room_id;
+	    DELETE FROM guest g where g.guest_id = guest_id;
 	END //
-	DELIMITER ;
-
+DELIMITER ;
+call delete_guest(8);
 -- Procedure to add RESERVATION
 	desc reservation;
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE MakeReservation(
 	    IN guest_id INT, 
 	    IN staff_id INT, 
@@ -99,10 +96,10 @@
 	    
 	    SET new_reservation_id = LAST_INSERT_ID();
 	END //
-	DELIMITER ;
+DELIMITER ;
 
 -- procedure to Assign Room to Reservation
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE AssignRoom(
 	    IN res_id INT, 
 	    IN room_id INT, 
@@ -120,14 +117,14 @@
 	    
 	    UPDATE Room SET status = 'Occupied' WHERE Room.room_id = room_id;
 	END //
-	DELIMITER ;
+DELIMITER ;
 	set @new_reservation_id = '';
 	call MakeReservation(8, 2, 1, @new_reservation_id);
 	call AssignRoom(@new_reservation_id, 1, '2025-03-31', '2025-04-03', 5);
 	call AssignRoom(@new_reservation_id, 2, '2025-03-31', '2025-04-03', 5);
 
 -- Procedure to cancel reservation
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE cancel_reservation(IN reservation_id INT)
 	BEGIN
 	    -- Step 1: Update the status of rooms back to "Available"
@@ -143,10 +140,10 @@
 	    -- Step 3: Delete the reservation itself
 	    DELETE FROM Reservation WHERE reservation_id = reservation_id;
 	END //
-	DELIMITER ;
+DELIMITER ;
 
 -- Delete assign Room in reservation
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE delete_assigned_room(
 	    IN res_id INT,
 	    IN room_id INT
@@ -161,10 +158,10 @@
 	    DELETE FROM Reservation_Room 
 	    WHERE reservation_id = res_id AND room_id = room_id;
 	END //
-	DELIMITER ;
+DELIMITER ;
 
 -- Procedure to proccess PAYMENT
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE ProcessPayment(
 	    IN res_id INT, 
 	    IN payment_method VARCHAR(100), 
@@ -175,22 +172,22 @@
 	    INSERT INTO Payment (payment_date, payment_method, amount_paid, trancation_id, status, reservation_id)
 	    VALUES (CURDATE(), payment_method, amount, trans_id, 'Completed', res_id);
 	END //
-	DELIMITER ;
+DELIMITER ;
 	
 	call ProcessPayment(@new_reservation_id, 'Cash', '100', 'TEST12345');
 
 -- Procedure refund payment
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE refund_payment(IN payment_id INT)
 	BEGIN
 	    UPDATE Payment 
 	    SET status = 'Refunded' 
 	    WHERE payment_id = payment_id;
 	END //
-	DELIMITER ;
+DELIMITER ;
 
 -- Procedure add review
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE add_review(
 	    IN guest_id INT,
 	    IN room_id INT,
@@ -201,17 +198,17 @@
 	    INSERT INTO Review (submission_date, guest_id, room_id, comment, rating)
 	    VALUES (CURDATE(), guest_id, room_id, comment, rating);
 	END //
-	DELIMITER ;
+DELIMITER ;
 -- Procedure delete review
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE delete_review(IN review_id INT)
 	BEGIN
 	    DELETE FROM Review WHERE review_id = review_id;
 	END //
-	DELIMITER ;
+DELIMITER ;
 
 -- Procedure add special request
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE add_special_request(
 	    IN res_id INT,
 	    IN request_type VARCHAR(100),
@@ -221,10 +218,10 @@
 	    INSERT INTO Special_Request (reservation_id, request_type, date_request, description, status)
 	    VALUES (res_id, request_type, CURDATE(), request_description, 'Pending');
 	END //
-	DELIMITER ;
+DELIMITER ;
 
 -- Procedure update special_request status
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE update_special_request(
 	    IN request_id INT,
 	    IN new_status VARCHAR(50)
@@ -234,18 +231,18 @@
 	    SET status = new_status
 	    WHERE request_id = request_id;
 	END //
-	DELIMITER ;
+DELIMITER ;
 
 -- Procedure delete special request
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE delete_special_request(IN request_id INT)
 	BEGIN
 	    DELETE FROM Special_Request WHERE request_id = request_id;
 	END //
-	DELIMITER ;
+DELIMITER ;
 
 -- Procedure add new staff
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE add_staff(
 	    IN staff_role VARCHAR(50),
 	    IN first_name VARCHAR(50),
@@ -260,10 +257,10 @@
 	    INSERT INTO Staff (staff_role, first_name, last_name, address, email, date_hire, contact_number, shift_schedule, admin_id)
 	    VALUES (staff_role, first_name, last_name, address, email, curdate(), contact_number, shift_schedule, admin_id);
 	END //
-	DELIMITER ;
+DELIMITER ;
 
 -- Procedure to update staff
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE update_staff(
 	    IN staff_id INT,
 	    IN staff_role VARCHAR(50),
@@ -287,67 +284,67 @@
 	        admin_id = admin_id
 	    WHERE staff_id = staff_id;
 	END //
-	DELIMITER ;
+DELIMITER ;
 
 -- Procedure to delete staff
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE delete_staff(IN staff_id INT)
 	BEGIN
 	    -- Directly delete the staff member; ON DELETE SET NULL will handle dependencies
 	    DELETE FROM Staff WHERE staff_id = staff_id;
 	END //
-	DELIMITER ;
+DELIMITER ;
 
 -- Procedure add amenity
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE add_amenity(IN amenity_name VARCHAR(100))
 	BEGIN
 	    INSERT INTO Amenities (amenity_name) VALUES (amenity_name);
 	END //
-	DELIMITER ;
+DELIMITER ;
 
 -- Procedure update amenity
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE update_amenity(IN amenity_id INT, IN new_name VARCHAR(100))
 	BEGIN
 	    UPDATE Amenities 
 	    SET amenity_name = new_name
 	    WHERE amenity_id = amenity_id;
 	END //
-	DELIMITER ;
+DELIMITER ;
 
 -- Procedure delete amenity
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE delete_amenity(IN amenity_id INT)
 	BEGIN
 	    DELETE FROM Amenities WHERE amenity_id = amenity_id;
 	END //
-	DELIMITER ;
+DELIMITER ;
 
 -- Procedure add amenity to room
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE assign_amenity_to_room(IN room_id INT, IN amenity_id INT)
 	BEGIN
 	    INSERT INTO Room_Amenity (room_id, amenity_id) VALUES (room_id, amenity_id);
 	END //
-	DELIMITER ;
+DELIMITER ;
 
 -- Procedure delete amenity to room
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE remove_amenity_from_room(IN room_id INT, IN amenity_id INT)
 	BEGIN
 	    DELETE FROM Room_Amenity 
 	    WHERE room_id = room_id AND amenity_id = amenity_id;
 	END //
-	DELIMITER ;
+DELIMITER ;
 
 -- Procedure to get available room
-	DELIMITER //
+DELIMITER //
 	CREATE PROCEDURE get_available_room()
 	BEGIN
 	    SELECT * FROM Room r WHERE r.status = 'available';
 	END //
-	DELIMITER ;
+DELIMITER ;
 call get_available_room();
 
 
